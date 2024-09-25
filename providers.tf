96482dd502bccbf5902407f7b7ebf2eed63339a6
Ricardo Amador <32242716+ricardoamador@users.noreply.github.com>
2024-09-24 21:52:27 -0700

    sf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,1 @@
-provider "google" {
-  project = "terraformlearning-435903"
-  region  = "us-west1"
-  zone    = "us-west1-a"
-}
+provider "google" { project = "terraformlearning-435903" region = "us-west1" zone = "us-west1-a" }
