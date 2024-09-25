215ef9301589a1cb56bf2be543f3c8c51a4f8632
Ricardo Amador <32242716+ricardoamador@users.noreply.github.com>
2024-09-24 22:13:04 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,6 @@
 provider "google" {
-  project = "terraformlearning-435903"
-  region  = "us-west1"
-  zone    = "us-west1-a" 
+  credentials = ""
+  project     = "terraformlearning-435903"
+  region      = "us-west1"
+  zone        = "us-west1-a" 
 }
